1b05f183a27c9e0db42cfe7f9d8903db1eb0631e
Rafael <rafaelvieirazv@gmail.com>
2017-01-15 16:44:10 -0200

    sdfdsf

--- myproject/myapp/tpICV3.py
+++ myproject/myapp/tpICV3.py
@@ -154,8 +154,8 @@ def classifyQuery(svms, imageName):
 
 	fp = open("/home/pokedexapp/pokedex-app/media/documents/2017/01/04/1.txt", 'w')
 	fp.close()
-	
-	call("./generatebic %s %s.txt" % (image, featuresPath), shell = True)
+
+	call("./home/pokedexapp/pokedex-app/generatebic %s %s.txt" % (image, featuresPath), shell = True)
 	features = readFeaturesQuery(featuresPath, pokemonData)
 
 	for i in svms:
